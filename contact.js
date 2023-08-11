3da06a5982edf646e60e49ef378d6deb9357baea
bolakecil <jeslynn30@gmail.com>
2023-08-11 18:17:33 +0700

    firebase gamau connect...

--- contact.js
+++ contact.js
@@ -7,3 +7,55 @@ function mobileMenu() {
     hamburger.classList.toggle("active");
     text.classList.toggle("active");
 }
+
+document.getElementById("button").onclick = function () {
+    let name = document.getElementById("name").value;
+    if (name == ""){
+        alert("Name should not be empty!");
+        return false;
+    }
+    else if (name.length < 3){
+        alert("Name must be more than 3 characters!");
+        return false;
+    }
+    else if (!(isNaN(name))){
+        alert("Name should only consist of letters!");
+        return false;
+    }
+
+    let companyname = document.getElementById("company-name").value;
+    
+    let email = document.getElementById("email").value;
+    if (email == ""){
+        alert("Email should not be empty!");
+        return false;
+    }
+    else if (email.split("@").length > 2 || email.split("@").length == 1) {
+        alert("Email should contain '@'!");
+        return false;
+    }
+    else if (!email.split("@")[1].includes(".com")) {
+        alert("Email should contain '.com' after '@'!");
+        return false;
+    }
+
+    let phone = document.getElementById("phone").value;
+    if (phone == ""){
+        alert("Phone number should not be empty!");
+        return false;
+    }
+    else if (phone.length < 6){
+        alert("Phone number must be more than 6 numbers!");
+        return false;
+    }
+    else if (isNaN(phone)){
+        alert("Phone number should only consist of numbers!");
+        return false;
+    }
+
+    let message = document.getElementById("message").value;
+    if (message == ""){
+        alert("Message should not be empty!");
+        return false;
+    }
+};
